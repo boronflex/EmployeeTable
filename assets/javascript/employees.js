61cb45f77f7cf901062e782394f0c41ed44b6484
Mark Kazanski <markkazanski@gmail.com>
2017-10-28 11:55:33 -0500

    got it working

--- assets/javascript/employees.js
+++ assets/javascript/employees.js
@@ -16,10 +16,64 @@ var config = {
   
   var database = firebase.database();
 
-  $("#submit").on("click", function(){ //
-    var nameInput = $("#nameInput").val();
+ /* $("#add-employee-btn").on("click", function(event){ //
+    event.preventDefault();
+    var nameInput = $("#employee-name-input").val();
     $("table").append(`<tr>
     <td>${nameInput}</td>
     <td></td>
     </tr>`);
+*/
+
+    var employeeName = "";
+  var role = "";
+  var startDate = "";
+  var monthlyRate = 0;
+
+ 
+  $('#add-employee-btn').on("click", function(event){
+    event.preventDefault();
+    employeeName = $('#employee-name-input').val().trim();
+        $('#employee-name-input').val("");
+    role = $('#role-input').val().trim();
+        $('#role-input').val("");
+    startDate = $('#start-input').val().trim(); 
+        $('#start-input').val("");
+    monthlyRate = $('#rate-input').val().trim();
+        $('#rate-input').val("");
+    database.ref().push({
+        employeeName: employeeName,
+        employeeRole: role,
+        startDate: startDate,
+        monthlyRate: monthlyRate,
+    });
+
+  /*  $("table tbody").append(`<tr>
+    <td>${employeeName}</td>
+    <td>${role}</td>
+    <td>${startDate}</td>
+    <td>Months Worked placeholder</td>
+    <td>${monthlyRate}</td>
+    <td>Total billed placeholder</td>
+    </tr>`);*/
+    database.ref().on("child_added", function(snapshot, prevChildKey){
+        var newName = snapshot.val().employeeName;
+        var newRole = snapshot.val().employeeRole;
+        var newDate = snapshot.val().startDate;
+        var newRate = snapshot.val().monthlyRate;
+        console.log( snapshot.val().employeeName );
+        $("table tbody").append(`<tr>
+        <td>${newName}</td>
+        <td>${newRole}</td>
+        <td>${newDate}</td>
+        <td>Months Worked</td>
+        <td>${newRate}</td>
+        <td>Total Billed</td>
+        </tr>`);
+    });
+
   });
+
+
+
+ 
